3443a92c16fd695f9fb4716f0c73ec4aa471cb3d
Jeston Lewis <61254261+jestoncolelewis@users.noreply.github.com>
2025-02-02 20:19:35 -0700

    completed cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,5 +1,28 @@
 def caeser_cipher(plaintext, shift_factor)
-  puts plaintext
+  numbers = Array (1..26)
+  lower_alpha = numbers.zip(Array ("a".."z")).to_h
+  upper_alpha = numbers.zip(Array ("A".."Z")).to_h
+  
+  encr_arr = Array.new
+
+  plaintext.chars.each do |char|
+    if /[[:upper:]]/.match(char) != nil
+      shifted_char = upper_alpha.key(char) + shift_factor
+      if shifted_char > 26
+        shifted_char -= 26
+      end
+      encr_arr.push(upper_alpha[shifted_char])
+    elsif /[[:lower:]]/.match(char) != nil
+      shifted_char = lower_alpha.key(char) + shift_factor
+      if shifted_char > 26
+        shifted_char -= 26
+      end
+      encr_arr.push(lower_alpha[shifted_char])
+    else
+      encr_arr.push(char)
+    end
+  end
+  encr_arr.join
 end
 
-caeser_cipher("Some text", 6)
+puts caeser_cipher("What a string!", 5)
